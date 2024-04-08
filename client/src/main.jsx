feb5d841ed0014d0fa357f7c0352f7e0511f6cb1
Mustafa Kanat <mkanat6620@gmail.com>
2024-04-08 15:37:48 +0300

    V1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import MainLayout from "./layouts/mainLayout.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 import { BrowserRouter } from "react-router-dom";
 import MessageProvider from "./providers/MessageProvider.jsx";
 
